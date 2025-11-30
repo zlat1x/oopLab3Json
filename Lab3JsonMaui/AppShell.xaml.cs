6f9e1f3aed457f5414d2b2abce2d52e21ed65627
Zlata Dovbyk <dovbykzlata@gmail.com>
2025-11-30 17:23:42 +0200

    feat(ui): add About page and shell navigation

--- Lab3JsonMaui/AppShell.xaml.cs
+++ Lab3JsonMaui/AppShell.xaml.cs
@@ -1,9 +1,12 @@
-﻿namespace Lab3JsonMaui;
-
-public partial class AppShell : Shell
+﻿namespace Lab3JsonMaui
 {
-	public AppShell()
-	{
-		InitializeComponent();
-	}
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute(nameof(AboutPage), typeof(AboutPage));
+        }
+    }
 }
