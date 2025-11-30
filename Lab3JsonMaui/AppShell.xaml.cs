20e02d56a359a4d5b4b126d0ed139f2a58ce0824
Zlata Dovbyk <dovbykzlata@gmail.com>
2025-11-30 16:27:33 +0200

    chore: init MAUI project for Lab3 JSON

---
+++ Lab3JsonMaui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab3JsonMaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Lab3JsonMaui/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Lab3JsonMaui")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Lab3JsonMaui.Pages")]
